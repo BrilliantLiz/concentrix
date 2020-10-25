2b978d82dbcc68a8d249324adf1ccadb90eff706
Elizaveta Shapiro <lizashap@gmail.com>
2020-10-24 20:51:17 -0600

    Initial commit

---
+++ ConcentrixHW/DemoSurveyHw.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConcentrixHW
+{
+    class DemoSurveyHw
+    {
+        static void Main()
+        {
+            try
+            {
+                Survey[] jsonData = (JsonConvert.DeserializeObject<Survey[]>
+                    (File.ReadAllText(@"../../../DemoSurveys_v1.1.json")));
+
+                Console.Clear();
+                Console.WriteLine("Total number of surveys is  " + jsonData.Length);
+
+                //Promoter surveys
+                int promotersSurveysNumber = 0;
+                foreach (Survey surv in jsonData)
+                {
+                    AnswerParent[] promotersSurveys = Array.FindAll(surv.Answers, sa => sa.QLabel == "DQNPS" && sa.ResponseText == "Promoter" &&
+                         surv.AnswerCount == 72);
+                    promotersSurveysNumber = promotersSurveys.Length != 0 ? promotersSurveysNumber + 1 : promotersSurveysNumber;
+                }
+                Console.WriteLine("Number of promoters surveys is " + promotersSurveysNumber);
+                Console.WriteLine("Percentage of promoters surveys is " +
+                   ((double)promotersSurveysNumber / (double)jsonData.Length).ToString("0.000"));
+
+                //Closed Cases
+                int closedCasesCount = 0;
+                foreach (Survey surv in jsonData)
+                {
+                    AnswerParent[] closedCases = Array.FindAll(surv.Answers, sa => sa.QLabel == "DQASTAT" && sa.ResponseText == "Closed");
+                    closedCasesCount = closedCases.Length != 0 ? closedCasesCount + 1 : closedCasesCount;
+                }
+
+                Console.WriteLine("Number of closed cases is " + closedCasesCount);
+                Console.WriteLine("Percentage of closed cases is " +
+                    ((double)closedCasesCount / (double)jsonData.Length).ToString("0.000%"));
+
+                // Survey 410
+                AnswerParent FXDTETME_data = Array.Find(jsonData[409].Answers, sa => sa.QLabel == "FXDTETME");
+                AnswerParent FXEMAIL_data = Array.Find(jsonData[409].Answers, sa => sa.QLabel == "FXEMAIL");
+                AnswerParent FXOSITE_data = Array.Find(jsonData[409].Answers, sa => sa.QLabel == "FXOSITE");
+                AnswerParent QOSR_data = Array.Find(jsonData[409].Answers, sa => sa.QLabel == "QOSR");
+
+                Console.WriteLine("Data for survey # 410: ");
+                Console.WriteLine("FXDTETME = " + Convert.ToDateTime(FXDTETME_data.StringValue).ToString("MM/dd/yyyy"));
+                Console.WriteLine("FXEMAIL = " + FXEMAIL_data.StringValue);
+                Console.WriteLine("FXOSITE = " + FXOSITE_data.StringValue);
+                Console.WriteLine("QOSR Comment = " + QOSR_data.Comment);
+
+
+                // Trend number of surveys by month
+                string[] monthNames = System.Globalization.CultureInfo.CurrentCulture.DateTimeFormat.MonthNames;
+                Survey[] Surveys_2020 = Array.FindAll(jsonData, jd => jd.CompleteDate.Value.Year == 2020);
+                Console.WriteLine("Monthly trends in 2020");
+                for (int i = 01; i <= 12; i++)
+                {
+                    int count = Surveys_2020.Count(s => s.CompleteDate.Value.Month == i);
+                    Console.WriteLine(monthNames[i - 1] + " - " + count);
+                }
+                Console.WriteLine("End of programm " + DateTime.Now);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ ConcentrixHW/Survey.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConcentrixHW
+{
+    public class DemoSurvey
+    {
+        public Survey[] Demo { get; set; }
+    }
+
+    public class Survey
+    {
+        public int RespondentId { get; set; }
+        public int? ShiftedRespNo { get; set; }
+        public DateTime? CompleteDate { get; set; }
+        public DateTime? TransactionDate { get; set; }
+        public DateTime? WhenChanged { get; set; }
+        public decimal? Weight1 { get; set; }
+        public int? AnswerCount { get; set; }
+        public AnswerParent[] Answers { get; set; }
+        
+    }
+
+    public class AnswerParent 
+    {
+        public string QLabel { get; set; }
+        public string ResponseText { get; set; }
+        public string KeyWord { get; set; }
+        public int? Punch { get; set; }
+        public string StringValue { get; set; }
+        public string Comment { get; set; }
+        public decimal? NumericValue { get; set; }
+        public AnswerChild[] Answers { get; set; }
+        public string AudioPath { get; set; }
+    }
+
+    public class AnswerChild
+    {
+        public string ResponseText { get; set; }
+        public string KeyWord { get; set; }
+        public string OtherSpecify { get; set; }
+        public string Punch { get; set; }
+    }
+}
+
+
